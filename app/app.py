d0a96b833fea73dd29608fbf4f92591eed19fe17
dastewart44 <david.a.stewart@insead.edu>
2023-06-07 14:42:46 +0200

    updated app.py

--- app/app.py
+++ app/app.py
@@ -1,25 +1,52 @@
 import streamlit as st
-import googlemaps
-import streamlit_folium
-import folium
 import requests
-from geopy.geocoders import Nominatim
+import pickle
+import io
+import zipfile
+import pandas as pd
+import tempfile
+import os
+    
+def best_schools(user_vals):
+    # Create a temporary directory
+    temp_dir = tempfile.gettempdir()
+    model_dir = os.path.join(temp_dir, 'models')
+    
+    # Check if models have already been downloaded
+    if not os.path.exists(model_dir) or len(os.listdir(model_dir)) < 176:
+        # URL for the zip file
+        url = 'https://storage.googleapis.com/school-finder-models/models.zip'
 
-def parse_address(address):
-    address_parts = address.split(",")
-    street = address_parts[0].strip()
-    city = "Denver"
-    state = "Colorado"
-    return street, city, state
+        # Download the zip file
+        response = requests.get(url)
+        response.raise_for_status()  # Ensure we got a successful response
 
-def geocode_place(street, city, state):
-    geolocator = Nominatim(user_agent="myGeocoder")
-    location = geolocator.geocode(f"{street}, {city}, {state}")
-    if location is not None:
-        return location.latitude, location.longitude
-    else:
-        print(f"Geocoding Error: Unable to locate {street}, {city}, {state}")
-        return None, None
+        # Save the zip file to disk
+        with open(f'{temp_dir}/models.zip', 'wb') as f:
+            f.write(response.content)
+
+        # Extract the models
+        with zipfile.ZipFile(f'{temp_dir}/models.zip', 'r') as zip_ref:
+            zip_ref.extractall(temp_dir)
+
+    # Now that the models are extracted, you can load each one and generate predictions
+    predictions = {}
+
+    for i in range(1, 177):
+        with open(f'{model_dir}/{i}_model.pkl', 'rb') as file:
+            pickle_model = pickle.load(file)
+            
+            # Make predictions
+            predict_val = pickle_model.predict(user_vals)
+            predictions[i] = predict_val[0]
+
+    predictions_df = pd.DataFrame.from_dict(predictions, orient='index', columns=['prediction'])
+
+    # Reset the index and name it 'school_id'
+    predictions_df = predictions_df.reset_index().rename(columns={'index': 'school_id'})
+    predictions_df = predictions_df.sort_values('prediction', ascending=False)
+
+    return predictions_df
 
 def main():
     st.title('Welcome to the (Fake) Denver School Finder')
@@ -27,21 +54,7 @@ def main():
     st.write('The goal of this school finder is to help you find the best school for your child.'
              'Answering the questions below will help us determine which school will help your child grow the most.')
 
-    address_input = st.text_input('What is your address?')
-            
-    if address_input:
-        street, city, state = parse_address(address_input)
-        latitude, longitude = geocode_place(street, city, state)
-        
-        # Create a folium map centered on the latitude and longitude
-        map = folium.Map(location=[latitude, longitude], zoom_start=12)
-
-        # Add a marker to the map at the latitude and longitude
-        folium.Marker([latitude, longitude], popup=address_input, icon=folium.Icon(color='blue')).add_to(map)
-        
-        # Display the map using streamlit-folium
-        streamlit_folium.folium_static(map)
-        
+    school_type = st.radio('What type of school are you looking for?', ('Elementary School', 'Middle School', 'High School'))       
     options = ['Special Education', 'Free/Reduced Lunch', 'English Language Learner']
     selected_options = st.multiselect('Please select the services your child receives:', options)
     race_ethnicity = st.radio("Please click on your child's race/ethnicity:", ('Asian', 'Black or African American', 'Hispanic or Latino', 'White', 'Other'))
@@ -68,16 +81,15 @@ def main():
     if 'English Language Learner' in selected_options :
         ell = 1
 
-    characteristics = [sped, frl, ell, race_vals[0], race_vals[1], race_vals[2], race_vals[3], starting_gpa]
-
-    st.write(characteristics)
+    characteristics = [[sped, frl, ell, race_vals[0], race_vals[1], race_vals[2], race_vals[3], starting_gpa]]
+    vals = pd.DataFrame(characteristics, columns=['sped_flag', 'frl_flag', 'ell_flag', 'asian_flag', 'black_flag', 'hispanic_flag', 'white_flag', 'starting_gpa'])
+    top_schools = best_schools(vals)
+    top_schools['SCHOOL_TYPE'] = school_type
     
-    if address_input:
-        return map
-    else :
-        pass
+    # Check if you've already initialized the data
+    if 'top_schools' not in st.session_state:
+        # Save the data to session state
+        st.session_state.df = top_schools
 
 if __name__ == "__main__":
     main()
-
-
