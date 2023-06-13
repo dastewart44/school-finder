aaf9debb25a7bc7c5907c70cf75574355cf6215a
dastewart44 <david.a.stewart@insead.edu>
2023-06-13 14:04:42 +0200

    updated page 1

--- app/pages/page_01.py
+++ app/pages/page_01.py
@@ -7,7 +7,7 @@ import pandas as pd
 import tempfile
 import os
 from streamlit_extras.switch_page_button import switch_page
-import sklearn
+from sklearn.ensemble import RandomForestClassifier
 
 st.set_page_config(page_icon=None, layout="centered", initial_sidebar_state="collapsed", menu_items=None)
 
